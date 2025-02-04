4ecaab17f89f2f81a028efca431a59dcca55c496
Tsion <tsiongemedab@gmail.com>
2025-02-03 20:57:27 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-2:c9645b9b-6f89-4977-9452-6fc27700fc36', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: 'arn:aws:cognito-identity:us-east-2:654654552436:identitypool/us-east-2:c9645b9b-6f89-4977-9452-6fc27700fc36', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-2_xCJOXCjXH', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '1ute9tjgkes807kpn9k2cacut1', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-2' // e.g. us-east-2
     },
     api: {
